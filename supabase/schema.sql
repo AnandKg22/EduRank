54e1dcfccb0fabf1399504f65e18d0affa3632fd
Anandkg22 <anandkg22@gmail.com>
2026-05-13 20:07:03 +0530

    fix(auth): map pre-seeded SuperAdmin user identity row correctly to target auth ID preventing REST layer 406 network errors

--- supabase/schema.sql
+++ supabase/schema.sql
@@ -360,7 +360,7 @@ INSERT INTO auth.users (
   instance_id, id, aud, role, email, encrypted_password, email_confirmed_at, recovery_sent_at, last_sign_in_at, raw_app_meta_data, raw_user_meta_data, created_at, updated_at, confirmation_token, recovery_token, email_change_token_new, email_change
 ) VALUES (
   '00000000-0000-0000-0000-000000000000',
-  'a1000000-0000-0000-0000-000000000000',
+  '91b1ec66-f31a-442f-b511-dd87b7ff8952',
   'authenticated',
   'authenticated',
   'anandkg22@gmail.com',
